ec632079b5b2bd1437fc4e87ff80b99bc3ce2689
Icarogamer2441 <148513434+Icarogamer2441@users.noreply.github.com>
2024-01-28 23:29:04 -0300

    Update universe.py

--- universe.py
+++ universe.py
@@ -17,6 +17,7 @@ G = 150
 drag_force_multiplier = 0.2
 mass_increase = 10
 black_hole_mass = 1000
+star_mass = 100  # Massa da estrela
 
 def calculate_gravity(obj1, obj2):
     distance = max(1, math.hypot(obj2['x'] - obj1['x'], obj2['y'] - obj1['y']))
@@ -26,7 +27,7 @@ def calculate_gravity(obj1, obj2):
     force_y = force * math.sin(angle)
     return force_x, force_y
 
-class Planet:
+class CelestialBody:
     def __init__(self, x, y, radius, color, mass=1):
         self.x = x
         self.y = y
@@ -51,52 +52,34 @@ class Planet:
     def increase_mass(self, amount):
         self.mass += amount
 
-class BlackHole:
-    def __init__(self, x, y, radius, color, mass):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.mass = mass
-
-    def draw(self):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
-
-    def apply_gravity(self, other_black_hole):
-        gravity_force = calculate_gravity(self.__dict__, other_black_hole.__dict__)
-        other_black_hole.apply_force((-gravity_force[0], -gravity_force[1]))
-
-    def apply_force(self, force):
-        acceleration_x = force[0] / self.mass
-        acceleration_y = force[1] / self.mass
-        self.x += acceleration_x
-        self.y += acceleration_y
-
 # Tela inicial
 font = pygame.font.Font(None, 24)
 text = font.render("PyUniverse", True, white)
 text_rect = text.get_rect(center=(width//2, height//2 - 100))
 
 info_text1 = font.render("Left click to create a planet", True, white)
-info_text1_rect = info_text1.get_rect(center=(width//2, height//2 - 50))
+info_text1_rect = info_text1.get_rect(center=(width//2, height//2 - 80))  # Ajuste na coordenada y
 
 info_text2 = font.render("Right click on a planet to increase its mass", True, white)
-info_text2_rect = info_text2.get_rect(center=(width//2, height//2))
+info_text2_rect = info_text2.get_rect(center=(width//2, height//2 - 40))  # Ajuste na coordenada y
 
 info_text3 = font.render("Press 'B' to create a black hole at the mouse position", True, white)
-info_text3_rect = info_text3.get_rect(center=(width//2, height//2 + 50))
+info_text3_rect = info_text3.get_rect(center=(width//2, height//2 + 10))  # Ajuste na coordenada y
 
 info_text4 = font.render("Press 'D' to delete all planets and black holes", True, white)
-info_text4_rect = info_text4.get_rect(center=(width//2, height//2 + 100))
+info_text4_rect = info_text4.get_rect(center=(width//2, height//2 + 60))  # Ajuste na coordenada y
 
 info_text5 = font.render("Drag with left click to apply a force to the planets", True, white)
-info_text5_rect = info_text5.get_rect(center=(width//2, height//2 + 150))
+info_text5_rect = info_text5.get_rect(center=(width//2, height//2 + 110))  # Ajuste na coordenada y
 
-info_text6 = font.render("G: 150 (Gravitational constant)", True, white)
-info_text6_rect = info_text6.get_rect(center=(width//2, height//2 + 200))
+info_text6 = font.render("Press 'S' to create a star", True, white)
+info_text6_rect = info_text6.get_rect(center=(width//2, height//2 + 160))  # Ajuste na coordenada y
 
-info_text7 = font.render("Press 'P' to start playing", True, white)
-info_text7_rect = info_text7.get_rect(center=(width//2, height//2 + 250))
+info_text7 = font.render("G: 150 (Gravitational constant)", True, white)
+info_text7_rect = info_text7.get_rect(center=(width//2, height//2 + 210))  # Ajuste na coordenada y
+
+info_text8 = font.render("Press 'P' to start playing", True, white)
+info_text8_rect = info_text8.get_rect(center=(width//2, height//2 + 260))  # Ajuste na coordenada y
 
 # Exibir tela inicial
 screen.fill(black)
@@ -108,6 +91,7 @@ screen.blit(info_text4, info_text4_rect)
 screen.blit(info_text5, info_text5_rect)
 screen.blit(info_text6, info_text6_rect)
 screen.blit(info_text7, info_text7_rect)
+screen.blit(info_text8, info_text8_rect)
 pygame.display.flip()
 
 waiting_for_start = True
@@ -125,13 +109,15 @@ while waiting_for_start:
 screen.fill(black)
 pygame.display.flip()
 
-planets = []
-black_holes = []
+celestial_bodies = []  # Lista para armazenar planetas, estrelas e buracos negros
 
 clock = pygame.time.Clock()
 dragging = False
 drag_start = (0, 0)
 drag_force = (0, 0)
+
+dragging_star = False
+star_drag_start = (0, 0)
 
 scroll_speed = 5
 center_arrow_size = 10
@@ -146,72 +132,67 @@ while True:
                 dragging = True
                 drag_start = pygame.mouse.get_pos()
             elif event.button == 3:
-                for planet in planets:
-                    distance = math.hypot(event.pos[0] - planet.x, event.pos[1] - planet.y)
-                    if distance <= planet.radius:
-                        planet.increase_mass(mass_increase)
+                for body in celestial_bodies:
+                    distance = math.hypot(event.pos[0] - body.x, event.pos[1] - body.y)
+                    if distance <= body.radius:
+                        body.increase_mass(mass_increase)
         elif event.type == pygame.MOUSEBUTTONUP:
             dragging = False
-            new_planet = Planet(event.pos[0], event.pos[1], 20, white)
-            planets.append(new_planet)
+            new_planet = CelestialBody(event.pos[0], event.pos[1], 20, white)
+            celestial_bodies.append(new_planet)
             new_planet.apply_force((drag_force[0] * drag_force_multiplier, drag_force[1] * drag_force_multiplier))
             drag_force = (0, 0)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_b:
                 # Cria um buraco negro nas coordenadas do mouse
-                black_hole = BlackHole(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 30, blue, black_hole_mass)
-                black_holes.append(black_hole)
+                black_hole = CelestialBody(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 30, blue, black_hole_mass)
+                celestial_bodies.append(black_hole)
+            elif event.key == pygame.K_s:
+                # Cria uma estrela nas coordenadas do mouse
+                star = CelestialBody(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 50, red, star_mass)
+                celestial_bodies.append(star)
             elif event.key == pygame.K_d:
-                # Deleta todos os planetas e buracos negros
-                planets = []
-                black_holes = []
+                # Deleta todos os corpos celestes
+                celestial_bodies = []
             elif event.key == pygame.K_LEFT:
-                for planet in planets:
-                    planet.x += scroll_speed
-                for black_hole in black_holes:
-                    black_hole.x += scroll_speed
+                for body in celestial_bodies:
+                    body.x += scroll_speed
             elif event.key == pygame.K_RIGHT:
-                for planet in planets:
-                    planet.x -= scroll_speed
-                for black_hole in black_holes:
-                    black_hole.x -= scroll_speed
+                for body in celestial_bodies:
+                    body.x -= scroll_speed
             elif event.key == pygame.K_UP:
-                for planet in planets:
-                    planet.y += scroll_speed
-                for black_hole in black_holes:
-                    black_hole.y += scroll_speed
+                for body in celestial_bodies:
+                    body.y += scroll_speed
             elif event.key == pygame.K_DOWN:
-                for planet in planets:
-                    planet.y -= scroll_speed
-                for black_hole in black_holes:
-                    black_hole.y -= scroll_speed
+                for body in celestial_bodies:
+                    body.y -= scroll_speed
+            elif event.key == pygame.K_s:
+                dragging_star = True
+                star_drag_start = pygame.mouse.get_pos()
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                dragging_star = False
+                current_pos = pygame.mouse.get_pos()
+                star.apply_force((current_pos[0] - star_drag_start[0], current_pos[1] - star_drag_start[1]))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        for planet in planets:
-            planet.x += scroll_speed
-        for black_hole in black_holes:
-            black_hole.x += scroll_speed
+        for body in celestial_bodies:
+            body.x += scroll_speed
     if keys[pygame.K_RIGHT]:
-        for planet in planets:
-            planet.x -= scroll_speed
-        for black_hole in black_holes:
-            black_hole.x -= scroll_speed
+        for body in celestial_bodies:
+            body.x -= scroll_speed
     if keys[pygame.K_UP]:
-        for planet in planets:
-            planet.y += scroll_speed
-        for black_hole in black_holes:
-            black_hole.y += scroll_speed
+        for body in celestial_bodies:
+            body.y += scroll_speed
     if keys[pygame.K_DOWN]:
-        for planet in planets:
-            planet.y -= scroll_speed
-        for black_hole in black_holes:
-            black_hole.y -= scroll_speed
+        for body in celestial_bodies:
+            body.y -= scroll_speed
 
     screen.fill(black)
 
-    center_x = sum(planet.x for planet in planets) / len(planets) if planets else width / 2
-    center_y = sum(planet.y for planet in planets) / len(planets) if planets else height / 2
+    center_x = sum(body.x for body in celestial_bodies) / len(celestial_bodies) if celestial_bodies else width / 2
+    center_y = sum(body.y for body in celestial_bodies) / len(celestial_bodies) if celestial_bodies else height / 2
 
     if center_x != width / 2 or center_y != height / 2:
         center_arrow_angle = math.atan2(height / 2 - center_y, width / 2 - center_x)
@@ -223,53 +204,27 @@ while True:
                                             (width / 2 + 5, height / 2 - 15),
                                             (width / 2, height / 2 - 5)])
 
-    for i in range(len(planets)):
-        for j in range(len(black_holes)):
-            black_holes[j].apply_gravity(planets[i])
+    for i in range(len(celestial_bodies)):
+        for j in range(len(celestial_bodies)):
+            if i != j:
+                gravity_force = calculate_gravity(celestial_bodies[i].__dict__, celestial_bodies[j].__dict__)
+                celestial_bodies[i].apply_force(gravity_force)
 
-    for i in range(len(planets)):
-        for j in range(i+1, len(planets)):
-            gravity_force = calculate_gravity(planets[i].__dict__, planets[j].__dict__)
-            planets[i].apply_force(gravity_force)
-            planets[j].apply_force((-gravity_force[0], -gravity_force[1]))
+    for body in celestial_bodies:
+        body.update()
+        body.draw()
 
-            distance = math.hypot(planets[j].x - planets[i].x, planets[j].y - planets[i].y)
-            if distance < planets[i].radius + planets[j].radius:
-                if planets[i].mass >= planets[j].mass:
-                    planets[i].mass += planets[j].mass
-                    planets.pop(j)
-                else:
-                    planets[j].mass += planets[i].mass
-                    planets.pop(i)
-                break
-
-    for i in range(len(black_holes)):
-        for j in range(i+1, len(black_holes)):
-            black_holes[i].apply_gravity(black_holes[j])
-
-            distance = math.hypot(black_holes[j].x - black_holes[i].x, black_holes[j].y - black_holes[i].y)
-            if distance < black_holes[i].radius + black_holes[j].radius:
-                if black_holes[i].mass >= black_holes[j].mass:
-                    black_holes[i].mass += black_holes[j].mass
-                    black_holes.pop(j)
-                else:
-                    black_holes[j].mass += black_holes[i].mass
-                    black_holes.pop(i)
-                break
-
-    for black_hole in black_holes:
-        black_hole.draw()
-
-    for planet in planets:
-        planet.update()
-        planet.draw()
-
-    for planet in planets:
-        pygame.draw.line(screen, red, (planet.x, planet.y), (planet.x + drag_force[0], planet.y + drag_force[1]), 3)
+    for body in celestial_bodies:
+        pygame.draw.line(screen, red, (body.x, body.y), (body.x + drag_force[0], body.y + drag_force[1]), 3)
 
     if dragging:
         current_pos = pygame.mouse.get_pos()
         drag_force = (current_pos[0] - drag_start[0], current_pos[1] - drag_start[1])
+
+    if dragging_star:
+        current_pos = pygame.mouse.get_pos()
+        star_drag_force = (current_pos[0] - star_drag_start[0], current_pos[1] - star_drag_start[1])
+        star.apply_force(star_drag_force)
 
     pygame.display.flip()
     clock.tick(60)
